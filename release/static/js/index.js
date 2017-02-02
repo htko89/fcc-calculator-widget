0fafa7329ae0469c06c8d3c46448e2f2a706cd7a
Tony Ko <htko89@gmail.com>
2017-02-02 18:39:45 -0500

    Add: Working calculation with watching for edge cases!

--- release/static/js/index.js
+++ release/static/js/index.js
@@ -1,63 +1,93 @@
 // final draft
 $( document ).ready( function() {
 
-  $( '.button' ).click( function() {
+  var buffer = "0";
+  var history = [];
+  var historyType = [];
+  var lastType = "number";
 
+  $( '.button' ).click( function button() {
+    // button event
     var event = {
       "type": $( this ).attr( "type" ),
       "value": $( this ).attr( "value" )
     };
-    $( '#status' ).html( event.type + " : " + event.value );
-    console.log( "Button event", event );
-
+    // case for event type
     switch ( event.type ) {
       case "function":
-        setFunction( event.value );
+        console.log( "function" );
+        switch ( event.value ) {
+          case "ac":
+            $( '#display #buffer' ).addClass( "active" );
+            buffer = "0";
+            history = [];
+            historyType = [];
+            lastType = "number";
+            break;
+          case "ce":
+            if( history.length > 0 ){
+              buffer = history[history.length-1];
+              lastType = historyType[historyType.length-1];
+              history.pop();
+              historyType.pop();
+            }
+            break;
+          case "=":
+            if(lastType === "number"){
+              $( '#display #buffer' ).addClass( "active" );
+              buffer = eval(history.join( "" )+buffer).toString(10);
+              history = [];
+              historyType = [];
+              lastType = "number";
+            }
+            break;
+        }
         break;
       case "operator":
-        setOperator( event.value );
+        console.log( "operator" );
+        if ( lastType !== "operator" ) {
+          $( '#display #buffer' ).removeClass( "active" );
+          buffer = parseFloat(buffer).toString(10);
+          history.push( buffer );
+          historyType.push( lastType );
+          lastType = "operator";
+          buffer = event.value;
+        } else {
+          buffer = event.value;
+        }
         break;
       case "number":
-        setNumber( event.value );
+        console.log( "number" );
+        if ( lastType !== "number" ) {
+          history.push( buffer )
+          historyType.push( lastType );
+          lastType = "number";
+          buffer = event.value;
+        } else {
+          if ( decimalSafe( event.value, buffer ) ) {
+            buffer += event.value;
+          }
+        }
+        if( buffer.slice(-1) !== "0" && buffer.slice(-1) !== "." ){
+          buffer = parseFloat(buffer).toString(10);
+        }
         break;
     }
+    // render
+    $( '#display #buffer' ).html( buffer );
+    $( '#display #history' ).html( history.join( "" ) + buffer );
+    console.log( history );
+    console.log( historyType );
+  } ); // end button function
 
-    function setFunction( str ) {
-      switch ( str ) {
-        case "ac":
-          $( '#display #primary' ).addClass("active");
-          $( '#display #primary' ).html( "0" );
-          $( '#display #secondary' ).html( "" );
-          break;
-        case "ce":
-          $( '#display #secondary' ).html( "" ); // change to erase last
-          break;
-        case "=":
-          $( '#display #secondary' ).html( "" ); // change to erase last
-          break;
+  function decimalSafe( str, buffer ) {
+    if ( str === "." ) {
+      if ( !buffer.includes( "." ) ) {
+        return true;
       }
+    } else {
+      return true;
     }
-
-    function setOperator( str ) {
-      $( '#display #primary' ).removeClass("active");
-    }
-
-    function setNumber( str ) {
-      var text = $( '#display #primary' ).text();
-      if ( str === "." ){
-        if ( !text.includes( "." ) ){
-          text += str;
-        }
-      } else {
-        if ( !text.includes( "." ) ){
-          text = text.replace(/^0+/, '') + "" + str;
-        } else {
-          text += str;
-        }
-      }
-      $( '#display #primary' ).html( text );
-    }
-
-  } );
-
-} ); // end doc ready function
+    return false;
+  }
+} ); // end doc ready
