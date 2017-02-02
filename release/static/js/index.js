d55e99b1adf1ef14ee267142e120ce92536ddc67
Tony Ko <htko89@gmail.com>
2017-02-01 23:46:17 -0500

    Add: Basic decimal checks, etc

--- release/static/js/index.js
+++ release/static/js/index.js
@@ -1,24 +1,63 @@
 // final draft
-$(document).ready(function() {
+$( document ).ready( function() {
 
-  $('.button').click(function() {
-      event = {
-        "type": $(this).attr("type"),
-        "value": $(this).attr("value")
-      };
-      console.log("Button event", event);
+  $( '.button' ).click( function() {
 
-      switch (event.type) {
-        case "function":
-          break;
-        case "operator":
-          break;
-        case "number":
-          break;
-      }
+    var event = {
+      "type": $( this ).attr( "type" ),
+      "value": $( this ).attr( "value" )
+    };
+    $( '#status' ).html( event.type + " : " + event.value );
+    console.log( "Button event", event );
 
+    switch ( event.type ) {
+      case "function":
+        setFunction( event.value );
+        break;
+      case "operator":
+        setOperator( event.value );
+        break;
+      case "number":
+        setNumber( event.value );
+        break;
     }
 
-  });
+    function setFunction( str ) {
+      switch ( str ) {
+        case "ac":
+          $( '#display #primary' ).addClass("active");
+          $( '#display #primary' ).html( "0" );
+          $( '#display #secondary' ).html( "" );
+          break;
+        case "ce":
+          $( '#display #secondary' ).html( "" ); // change to erase last
+          break;
+        case "=":
+          $( '#display #secondary' ).html( "" ); // change to erase last
+          break;
+      }
+    }
 
-}); // end doc ready function
+    function setOperator( str ) {
+      $( '#display #primary' ).removeClass("active");
+    }
+
+    function setNumber( str ) {
+      var text = $( '#display #primary' ).text();
+      if ( str === "." ){
+        if ( !text.includes( "." ) ){
+          text += str;
+        }
+      } else {
+        if ( !text.includes( "." ) ){
+          text = text.replace(/^0+/, '') + "" + str;
+        } else {
+          text += str;
+        }
+      }
+      $( '#display #primary' ).html( text );
+    }
+
+  } );
+
+} ); // end doc ready function
